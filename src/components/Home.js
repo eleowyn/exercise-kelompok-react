a93724b7710b3442f9873e79746e13c920331c8d
Lana Londah <S22310372@student.unklab.ac.id>
2024-11-05 11:02:51 +0800

    adding home and header logic

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import cardinfo from "./Home/cardinfo";
+import search from "./Home/search";
+
+const Home = () => {
+    return (
+        <div>
+            <cardinfo></cardinfo>
+            <search></search>
+        </div>
+        
+    )
+};
+
+export default Home;
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import LOGO from "./header/LOGO";
+import Generations from "./header/Generations";
+
+const header = () => {
+    return (
+        <div>
+            <LOGO></LOGO>
+            <Generations></Generations>
+        </div>
+        
+    )
+};
+
+export default header;
